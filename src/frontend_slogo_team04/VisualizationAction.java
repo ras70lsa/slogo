4ecf7e0ad14d020bd7924ea83532efb7e4904257
Sophie Guo <yg37@duke.edu>
2016-02-25 15:13:51 -0500

    added visual turtle

---
+++ src/frontend_slogo_team04/VisualizationAction.java
@@ -0,0 +1,28 @@
+package frontend_slogo_team04;
+
+import java.util.List;
+
+import backend_slogo_team04.Action;
+
+public interface VisualizationAction {
+		public double forward(double pixels);
+	    public double back(double pixels);
+	    public double left(double pixels);
+	    public double right(double pixels);
+	    public double setHeading(double degrees);
+	    public double towards(double x, double y);
+	    public double setxy(double x, double y);
+	    public double penDown();
+	    public double penUp();
+	    public double showTurtle();
+	    public double hideTurtle();
+	    public double home();
+	    public double clearScreen();
+	    public double xCor();
+	    public double yCor();
+	    public double heading();
+	    public boolean isPenDown();
+	    public boolean isShowing();
+	    public void updateHistory(List<Action> history);
+
+}
