50d40876f2c9401bdef600904f7569f3156b7b6e
Alexander <xanderyst@gmail.com>
2016-02-25 01:56:16 -0500

    Added Options Button

--- src/frontend_slogo_team04/Display.java
+++ src/frontend_slogo_team04/Display.java
@@ -10,6 +10,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 /**
@@ -79,7 +83,13 @@ public class Display {
 	private void createMenuBar() {
 		
 		Menu file = new Menu("Options");
+		Menu help = new Menu("Help");
+		MenuItem commands = new MenuItem("Commands");
+		help.getItems().add(commands);
+		commands.setOnAction(e->helpBox("Help"));
+		
 		MenuBar bar = new MenuBar(file);
+		bar.getMenus().add(help);
 		loopAndDo(m -> addItem(m, file));
 		panes.getChildren().add(bar);
 		
@@ -94,6 +104,33 @@ public class Display {
 	public void start() {
 		myStage.show();
 	}
+	
+	private Stage helpBox (String title) {
+		 Stage stage = new Stage();
+		 stage.setTitle("HTML");
+	     stage.setWidth(500);
+	     stage.setHeight(500);
+	     Scene scene = new Scene(new Group());
+	     VBox root = new VBox();     
+
+	     final WebView browser = new WebView();
+	     final WebEngine webEngine = browser.getEngine();
+	     ScrollPane scrollPane = new ScrollPane();
+	     scrollPane.setContent(browser);
+	     scrollPane.setFitToHeight(true);
+	     scrollPane.setFitToWidth(true);
+	     ClassLoader classLoader = getClass().getClassLoader();
+	 	 String url = classLoader.getResource("visual_resources/Commands.html").toExternalForm();  
+	     webEngine.load(url);
+	     
+	     root.getChildren().addAll(scrollPane);
+	     scene.setRoot(root);
+	     
+	     stage.setScene(scene);
+	     stage.show();
+	     return stage;
+	        
+   }
 	
 	public void setUpScene() {
 		
