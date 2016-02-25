920a422099de1542f9e0847d90df6313d0d40442
Sophie Guo <yg37@duke.edu>
2016-02-25 15:32:04 -0500

    added width and height constant

--- src/constants/DisplayConstants.java
+++ src/constants/DisplayConstants.java
@@ -5,5 +5,8 @@ import javafx.scene.paint.Color;
 public class DisplayConstants {
 
 	public static final int DISPLAY_SIZE = 500;
+	public static final int VIEW_HEIGHT = 300;
+	public static final int VIEW_WIDTH = 250;
 	public static final Color BACKGROUND_COLOR = Color.WHITE;
+	
 }
