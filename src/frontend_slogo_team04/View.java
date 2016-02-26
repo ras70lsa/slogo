d8f8eb2c9ad1db475598b65a32613c253b068e39
Sophie Guo <yg37@duke.edu>
2016-02-26 02:31:33 -0500

    added moveTurtle and other visualization methods

--- src/frontend_slogo_team04/View.java
+++ src/frontend_slogo_team04/View.java
@@ -17,14 +17,13 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
-public class View extends StaticPane implements VisualizationAction {
+public class View extends StaticPane{
 
 	private Controller myController;
 	private VisualTurtle turtle;
 	private TestingState state;
 	private boolean penDown = true;
 	private boolean isShowing = true;
-	private double scaleFactor = 1;
 
 	public View(TestingState state) {
 		this.state = state;
@@ -35,24 +34,140 @@ public class View extends StaticPane implements VisualizationAction {
 		state.getColorProperty().addListener((a, b, newValue) -> updateColor(newValue));
 	}
 
-	public void draw() {
-
-	}
-
 	public void setUp() {
 		// TODO Auto-generated method stub
 		Pane newDisplay = new Pane();
 		newDisplay.setStyle(state.getBackgroundColor());
 		newDisplay.setPrefSize(DisplayConstants.VIEW_WIDTH, DisplayConstants.VIEW_HEIGHT);
 		setPane(newDisplay);
-		setTurtleImage(new Image(getClass().getClassLoader().getResourceAsStream("/slogo_team04/images/turtle.jpg")));
+		setTurtleImage(new Image(getClass().getClassLoader().getResourceAsStream("turtle.jpg")));
 		add(turtle, getCenterXCor(),  getCenterYCor());
 	}
 
 
+	@Override
+	public void setUp() {
+		Pane newDisplay = new Pane();
+		newDisplay.setStyle(state.getBackgroundColor());
+		newDisplay.setPrefSize(DisplayConstants.VIEW_WIDTH, DisplayConstants.VIEW_HEIGHT);
+		setPane(newDisplay);
+		setTurtleImage(new Image(getClass().getClassLoader().getResourceAsStream("turtle.gif")));
+		add(turtle, getCenterXCor(), getCenterYCor());
+		// add(new Circle(10), getCenterXCor(), getCenterYCor());
+	}
+
+	public void draw(double endX, double endY) {
+		Line line = new Line();
+		line.setStartX(turtle.getTranslateX());
+		line.setStartY(turtle.getTranslateY());
+		Timeline timeline = new Timeline();
+		timeline.setCycleCount(1);
+		timeline.getKeyFrames().add(createKeyFrame(line.startXProperty(), line.startYProperty(), endX, endY));
+		timeline.play();
+		lineManager.add(line);
+	}
+
+	public void moveTurtle(double endX, double endY) {
+		Timeline timeline = new Timeline();
+		timeline.setCycleCount(1);
+		timeline.getKeyFrames().add(createKeyFrame(turtle.xProperty(), turtle.yProperty(), endX, endY));
+		timeline.play();
+	}
+
+	public KeyFrame createKeyFrame(DoubleProperty startX, DoubleProperty startY, double endX, double endY) {
+		KeyValue kv_x = new KeyValue(startX, endX, Interpolator.LINEAR);
+		KeyValue kv_y = new KeyValue(startY, endY, Interpolator.LINEAR);
+		KeyFrame kf = new KeyFrame(Duration.millis(INTERVAL_LENGTH), kv_x, kv_y);
+		return kf;
+	}
+
+	public void turn(double newHeading) {
+		RotateTransition rt = new RotateTransition(Duration.millis(INTERVAL_LENGTH), turtle);
+		rt.setByAngle(newHeading - getHeading());
+		rt.play();
+	}
+
+	public double setHeading(double degrees) {
+		turtle.setHeading(degrees);
+		return 0;
+	}
+
+	public double getHeading() {
+		return turtle.getHeading();
+	}
+
+	public double setxy(double x, double y) {
+		turtle.setTranslateX(x);
+		turtle.setTranslateY(y);
+		return 0;
+	}
+
+	public double penDown() {
+		penDown = true;
+		return 1;
+	}
+
+	public double penUp() {
+		penDown = false;
+		return 0;
+	}
+
+	public double showTurtle() {
+		turtle.showTurtle();
+		return 1;
+	}
+
+	public double hideTurtle() {
+		turtle.hideTurtle();
+		return 0;
+	}
+
+	public double home() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double clearScreen() {
+		resetTurtlePosition();
+
+		return 0;
+	}
+
+	public void resetTurtlePosition() {
+		turtle.setTranslateX(getCenterXCor());
+		turtle.setTranslateY(getCenterYCor());
+		lineManager.forEach(l -> l.setVisible(false));
+		lineManager.clear();
+	}
+
+	public double xCor() {
+		return turtle.getTranslateX() - getCenterXCor();
+	}
+
+	public double yCor() {
+		return getCenterYCor() - turtle.getTranslateY();
+	}
+
+	public double getCenterXCor() {
+		return DisplayConstants.VIEW_WIDTH / 2;
+	}
+
+	public double getCenterYCor() {
+		return DisplayConstants.VIEW_HEIGHT / 2;
+	}
+
+	public double heading() {
+		return turtle.getHeading();
+	}
+
+	@Override
+	public State getState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	private void setTurtleImage(Image i) {
 		turtle.setImage(i);
-		
 	}
 
 	public double getMaxWidth() {
@@ -63,138 +178,13 @@ public class View extends StaticPane implements VisualizationAction {
 		return DisplayConstants.VIEW_HEIGHT;
 	}
 
-	@Override
-	public double forward(double pixels) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-
-	public double getScaleFactor() {
-		return scaleFactor;
-	}
-
-	@Override
-	public double back(double pixels) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double left(double degrees) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
 	public boolean isPenDown() {
-		// TODO Auto-generated method stub
 		return penDown;
 	}
 
-	@Override
 	public boolean isShowing() {
-		// TODO Auto-generated method stub
 		return isShowing;
 	}
-
-	public State getState() {
-		return state;
-	}
-
-
-	@Override
-	public double right(double pixels) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double setHeading(double degrees) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double towards(double x, double y) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double setxy(double x, double y) {
-		// TODO Auto-generated method stub
-		turtle.setTranslateX(x);
-		turtle.setTranslateY(y);
-		return 0;
-	}
-
-	@Override
-	public double penDown() {
-		// TODO Auto-generated method stub
-		penDown = true;
-		return 1;
-	}
-
-	@Override
-	public double penUp() {
-		// TODO Auto-generated method stub
-		penDown = false;
-		return 0;
-	}
-
-	@Override
-	public double showTurtle() {
-		turtle.showTurtle();
-		return 1;
-	}
-
-	@Override
-	public double hideTurtle() {
-		turtle.hideTurtle();
-		return 0;
-	}
-
-	@Override
-	public double home() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double clearScreen() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double xCor() {
-		return turtle.getTranslateX() - getCenterXCor();
-	}
-	
-	@Override
-	public double yCor() {
-		return getCenterYCor() - turtle.getTranslateY();
-	}
-
-	public double getCenterXCor(){
-		return DisplayConstants.VIEW_WIDTH/2;
-	}
-	
-	public double getCenterYCor(){
-		return DisplayConstants.VIEW_HEIGHT/2;
-	}
-	
-	@Override
-	public double heading() {
-		return turtle.getHeading();
-	}
-	
-	@Override
-	public void updateHistory(List<Action> history) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
+
 
