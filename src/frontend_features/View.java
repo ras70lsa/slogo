67157957c5ee9b98e783fd2d32c5464f7c4c5f7f
Sophie Guo <yg37@duke.edu>
2016-02-27 12:21:51 -0500

    deleted animation

--- src/frontend_features/View.java
+++ src/frontend_features/View.java
@@ -1,6 +1,5 @@
 package frontend_features;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -38,43 +37,44 @@ import javafx.util.Duration;
 import model.Model;
 import model.ViewModel;
 
-public class View extends StaticPane implements Observer{
+public class View extends StaticPane implements Observer {
 
 	private VisualTurtle turtle;
 	private double scaleFactor = 1;
 	private ViewUIState visuals;
 	private ViewModel model;
+	private Pen pen;
 	private Stack<Line> lineManager;
 	private static final double INTERVAL_LENGTH = 1000;
-	
+
 	public View(IView model) {
 		turtle = new VisualTurtle(getFirstImage());
 		this.model = (ViewModel) model;
 		addCSS("visual_resources/DefaultView.css");
 		visuals = new ViewUIState();
 		lineManager = new Stack<Line>();
+		pen = new Pen(Color.BLACK);
 		addSilentListeners();
 		model.addObserver(this);
 		setUp();
 	}
-	
+
 	private Image getFirstImage() {
 		return new Image(getClass().getClassLoader().getResourceAsStream("visual_resources/turtle.jpg"));
 	}
 
 	private void addSilentListeners() {
-		visuals.getImageProperty().addListener((a,b,c) -> setTurtleImage(c));
-		
+		visuals.getImageProperty().addListener((a, b, c) -> setTurtleImage(c));
+
 	}
 
 	public void setUp() {
-		add(turtle, getCenterXCor(turtle.getFitWidth()),getCenterYCor(turtle.getFitHeight()));
+		add(turtle, getCenterXCor(turtle.getFitWidth()), getCenterYCor(turtle.getFitHeight()));
 	}
-
 
 	private void setTurtleImage(Image i) {
 		turtle.setImage(i);
-		
+
 	}
 
 	public double getMaxWidth() {
@@ -85,25 +85,22 @@ public class View extends StaticPane implements Observer{
 		return DisplayConstants.VIEW_HEIGHT;
 	}
 
-
 	public double getScaleFactor() {
 		return scaleFactor;
 	}
-	
 
 	public State getState() {
 		return null;
 	}
 
+	public double getCenterXCor(double imageWidth) {
+		return DisplayConstants.VIEW_WIDTH / 2 - (imageWidth / 2);
 
-	public double getCenterXCor(double imageWidth){
-		return DisplayConstants.VIEW_WIDTH/2-(imageWidth/2);
-		
 	}
-	
-	public double getCenterYCor(double imageHeight){
-		return DisplayConstants.VIEW_HEIGHT/2-(imageHeight/2);
-		
+
+	public double getCenterYCor(double imageHeight) {
+		return DisplayConstants.VIEW_HEIGHT / 2 - (imageHeight / 2);
+
 	}
 
 	@Override
@@ -112,91 +109,73 @@ public class View extends StaticPane implements Observer{
 		list.add(factory.get(visuals.getImageProperty(), "Testing"));
 		return list;
 	}
-	
+
 	@Override
 	public void update(Observable o, Object arg) {
 		double modelX = model.xCor();
 		double modelY = model.yCor();
-		if (model.isPenDown() == 1){
-			turn(model.getRotation());
-			moveTurtle(modelX, modelY);
-		}else{
-			updateTurtleAndDraw(modelX, modelY);
+		if (model.isPenDown() != 1) {
+			drawLine(turtle.getTranslateX(), turtle.getTranslateX(), translateToLineX(modelX),
+					translateToLineY(modelY));
 		}
+		turn(model.getRotation());
+		moveTurtle(translateToTurtleX(modelX), translateToTurtleY(modelY));
+	}
+
+	public void drawLine(double startX, double startY, double endX, double endY) {
+		Line newLine = pen.createLine(startX, startY, endX, endY);
+		add(newLine, startX, startY);
+		lineManager.add(newLine);
 	}
 
 	public double home() {
-		updateTurtleAndDraw(getCenterXCor(turtle.getFitWidth()),getCenterYCor(turtle.getFitHeight()));
+		moveTurtle(getCenterXCor(turtle.getFitWidth()), getCenterYCor(turtle.getFitHeight()));
 		return 0;
 	}
-	
+
 	public double clearScreen() {
-		lineManager.forEach(l -> l.setVisible(false));
-		lineManager.clear();
+		getLineManager().forEach(l -> l.setVisible(false));
+		getLineManager().clear();
 		resetTurtlePosition();
 		return 0;
 	}
-	
+
 	public void resetTurtlePosition() {
 		turtle.setTranslateX(getCenterXCor(turtle.getFitWidth()));
 		turtle.setTranslateY(getCenterYCor(turtle.getFitHeight()));
 	}
-	
-	public void updateTurtleAndDraw(double modelX, double modelY) {
-		Line line = new Line();
-		//add(line,calculateLineXCor(model.getHistory(),model.getPreviousYCor()));
-		//add(line,10,10);
-		Timeline timeline = new Timeline();
-		timeline.getKeyFrames()
-		.add(createKeyFrame(line.startXProperty(), line.startYProperty(), translateToLineX(modelX), translateToLineY(modelY)));
-		timeline.getKeyFrames()
-		.add(createKeyFrame(turtle.xProperty(), turtle.yProperty(), translateToTurtleX(modelX), translateToTurtleY(modelY)));
-		timeline.play();
-		lineManager.add(line);
-	}
-	
-	private Stack<Line> getLineManager(){
+
+	private Stack<Line> getLineManager() {
 		return lineManager;
 	}
-	
-	public void moveTurtle(double modelX, double modelY) {
-		Timeline timeline = new Timeline();
-		timeline.getKeyFrames().add(createKeyFrame(turtle.xProperty(), turtle.yProperty(), translateToTurtleX(modelX), translateToTurtleY(modelY)));
-		timeline.play();
-	}
-	
-	public KeyFrame createKeyFrame(DoubleProperty startX, DoubleProperty startY, double endX, double endY) {
-		KeyValue kv_x = new KeyValue(startX, endX, Interpolator.LINEAR);
-		KeyValue kv_y = new KeyValue(startY, endY, Interpolator.LINEAR);
-		KeyFrame kf = new KeyFrame(Duration.millis(INTERVAL_LENGTH), kv_x, kv_y);
-		return kf;
-	}
-	
-	public void turn(double rotation) {
-		RotateTransition rt = new RotateTransition(Duration.millis(INTERVAL_LENGTH), turtle);
-		rt.setByAngle(rotation);
-		rt.play();
+
+	public void moveTurtle(double endX, double endY) {
+		turtle.setTranslateX(endX);
+		turtle.setTranslateY(endY);
 	}
 
-	private double translateToLineX(double xCor){
+	public void turn(double rotation) {
+		turtle.setRotate(rotation);
+	}
+
+	private double translateToLineX(double xCor) {
 		return xCor + getCenterXCor(turtle.getFitHeight());
 	}
-	
-	public double translateToLineY(double yCor){
+
+	public double translateToLineY(double yCor) {
 		return getCenterYCor(turtle.getFitHeight()) - yCor;
 	}
-	
-	public double translateToTurtleX(double xCor){
+
+	public double translateToTurtleX(double xCor) {
 		return xCor;
 	}
-	
-	public double translateToTurtleY(double yCor){
+
+	public double translateToTurtleY(double yCor) {
 		return -yCor;
 	}
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+
 	}
-	
 }
 
