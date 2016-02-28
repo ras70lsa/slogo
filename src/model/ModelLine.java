6c2f4d2d34f00b4b47ddfc8223fbad28ef7640cd
Sophie Guo <yg37@duke.edu>
2016-02-27 21:35:39 -0500

    added modelline

---
+++ src/model/ModelLine.java
@@ -0,0 +1,21 @@
+package model;
+
+public class ModelLine {
+	private double endX, endY;
+	
+	public ModelLine(double endX, double endY){
+		this.endX = endX;
+		this.endY = endY;
+	}
+	
+	public double getEndX(){
+		return endX;
+	}
+	
+	public double getEndY(){
+		return endY;
+	}
+	
+	
+	
+}
