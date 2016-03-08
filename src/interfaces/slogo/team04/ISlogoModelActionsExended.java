efec409418881687f2abf12967249d62041b4aa0
Jon Im <imisjonathan@gmail.com>
2016-03-07 19:17:22 -0500

    Added required methods to our interfaces to support extension backend features

---
+++ src/interfaces/slogo/team04/ISlogoModelActionsExended.java
@@ -0,0 +1,76 @@
+package interfaces.slogo.team04;
+import java.util.List;
+
+import backend.slogo.team04.Action;
+
+
+/**
+ * Adding support for multiple turtles
+ * @author jonathanim
+ *
+ */
+public interface ISlogoModelActionsExended extends ISlogoModelActions{
+    double forward(double pixels, double turtleID);
+    double back(double pixels, double turtleID);
+    double left(double pixels, double turtleID);
+    double right(double pixels, double turtleID);
+    double setHeading(double degrees, double turtleID);
+    double towards(double x, double y, double turtleID);
+    double setxy(double x, double y, double turtleID);
+    double penDown(double turtleID);
+    double penUp(double turtleID);
+    double showTurtle(double turtleID);
+    double hideTurtle(double turtleID);
+    double home(double turtleID);
+    double isPenDown(double turtleID);
+    double isShowing(double turtleID);
+    double clearScreen();
+    void update();
+    double xCor(double turtleID);
+    double yCor(double turtleID);
+    double heading(double turtleID);
+    
+    /**
+     * A boolean for each turtle representing whether or not it is active 
+     * @return
+     */
+    boolean[] activeTurtles();
+    
+    double setBackground(int index);
+    double setPenColor(int index);
+    double setPenSize(double pixels);
+    double setShape(int index);
+    
+    
+    double setPalette(int index, int r, int g, int b);
+    double penColor();
+    double shape();
+    double stamp();
+    double clearStamps();
+    
+    double ID();
+    double turtles();
+    
+    /**
+     * I think we should include the syntatic sugar that will fill in the array, ie, if you try to create tell [ 100 ] 
+     * then our code will make sure that turtles 1 - 100 exist
+     * 
+     * Newly created turtles will be set to active, otherwise, after calling this command only the turtles in the list 
+     * should be active, alll non-listed should be set to false
+     * @return
+     */
+    double tell(int[] arrayOfActiveTurtleIDs);
+    
+    /**
+     * This saves the current list of active turtles to be restored later by popCurrentActive
+     */
+    void pushCurrentActive();
+    
+    void popCurrentActive();
+    
+    
+    
+    
+    
+
+}
