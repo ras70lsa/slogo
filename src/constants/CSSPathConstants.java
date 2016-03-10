3229c9793a452235ec5d1a2ee7eebc0270a95f6d
Alexander <xanderyst@gmail.com>
2016-03-10 16:41:35 -0500

    Refactored help packages

--- src/constants/CSSPathConstants.java
+++ src/constants/CSSPathConstants.java
@@ -1,11 +1,11 @@
 package constants;
-
 public class CSSPathConstants {
 
 	public static final String PATH = "css/";
+	public static final String HTML_PATH = "help/";
 	public static final String LIST = PATH + "DefaultList.css";
-	public static final String HELP = PATH + "Commands.html";
-	public static final String HELP_EXTENDED = PATH + "CommandsExtended.html";
+	public static final String HELP = HTML_PATH + "Commands.html";
+	public static final String HELP_EXTENDED = HTML_PATH + "CommandsExtended.html";
 	public static final String INPUT = PATH + "DefaultInputBox.css";
 	public static final String GRID_PANE = PATH + "GridPane.css";
 	public static final String DEFAULT_VIEW = PATH + "DefaultView.css";
